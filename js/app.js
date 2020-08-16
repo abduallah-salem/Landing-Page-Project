1b9fa3433e9a53359a8e3beb4aa1d5d19d3037d9
Viperboya <viperboy_a@hotmail.co.uk>
2020-08-16 19:25:08 +0200

    navBuilder + Intersecting Sections

--- js/app.js
+++ js/app.js
@@ -11,33 +11,47 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 let mSections = document.querySelectorAll('section');
 let nSections = mSections.length;
 const navBar = document.getElementById('navbar__list');
+let observer;
+let Options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: [0.0, 0.75]
+};
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-
+ */
+function changeClassOnIntersection(entries) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting && entry.intersectionRatio >= 0.7 && entry.target.className != 'your-active-class') {
+            document.querySelector('.your-active-class').classList.remove('your-active-class');
+            entry.target.className = 'your-active-class';
+        }
+    })
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-function navBuilder(sections, nSections) {
+function navBuilder(sections) {
     let dFragment = document.createDocumentFragment();
-    for(let section of sections){
+    for (let section of sections) {
         const listItem = document.createElement('li');
         listItem.className = 'menu__link';
         listItem.textContent = section.querySelector('.landing__container h2').textContent;
@@ -56,12 +70,13 @@ function navBuilder(sections, nSections) {
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
-navBuilder(mSections, nSections)
+navBuilder(mSections)
 // Scroll to section on link click
-
+observer = new IntersectionObserver(handleIntersection,Options);
+for(let section of mSections){
+observer.observe(section);
+}
 // Set sections as active
-
-
