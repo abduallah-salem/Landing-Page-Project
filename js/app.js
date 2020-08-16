1718bb0f95324034fd077bf79cad3349d94dd398
Viperboya <viperboy_a@hotmail.co.uk>
2020-08-17 00:13:58 +0200

    navBuilder + Intersecting Sections + scrollTo + active nav item

--- js/app.js
+++ js/app.js
@@ -18,13 +18,12 @@
  * 
  */
 let mSections = document.querySelectorAll('section');
-let nSections = mSections.length;
-const navBar = document.getElementById('navbar__list');
+let navBar = document.getElementById('navbar__list');
 let observer;
 let Options = {
     root: null,
     rootMargin: "0px",
-    threshold: [0.0, 0.75]
+    threshold: [0.0, 0.25, 0.5, 0.75]
 };
 
 /**
@@ -34,9 +33,14 @@ let Options = {
  */
 function changeClassOnIntersection(entries) {
     entries.forEach(function (entry) {
-        if (entry.isIntersecting && entry.intersectionRatio >= 0.7 && entry.target.className != 'your-active-class') {
+        if (entry.isIntersecting && entry.intersectionRatio >= 0.51 && entry.target.className != 'your-active-class') {
             document.querySelector('.your-active-class').classList.remove('your-active-class');
             entry.target.className = 'your-active-class';
+            let navBarActive = navBar.querySelector('.activeSection');
+            if (navBarActive != null) {
+                navBarActive.classList.remove('activeSection');
+            }
+            navBar.querySelector(`li[data-nav="${entry.target.getAttribute('data-nav')}"]`).classList.add('activeSection');
         }
     })
 }
@@ -51,20 +55,32 @@ function changeClassOnIntersection(entries) {
 // build the nav
 function navBuilder(sections) {
     let dFragment = document.createDocumentFragment();
-    for (let section of sections) {
+    for (let [index, section] of sections.entries()) {
         const listItem = document.createElement('li');
-        listItem.className = 'menu__link';
+        if(index == 0){
+            listItem.classList.add('activeSection');
+        }
+        listItem.classList.add('menu__link');
         listItem.textContent = section.querySelector('.landing__container h2').textContent;
+        listItem.setAttribute('data-nav', section.getAttribute('data-nav'))
         dFragment.appendChild(listItem);
     }
     navBar.append(dFragment);
+    navBar = document.getElementById('navbar__list');
 }
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSection(e) {
+    let targetSection = document.querySelector(`section[data-nav="${e.target.getAttribute('data-nav')}"]`);
+    window.scrollTo({
+        top: targetSection.offsetTop - navBar.offsetHeight,
+        left: targetSection.offsetLeft,
+        behavior: 'smooth'
+    });
+}
 
 /**
  * End Main Functions
@@ -73,10 +89,12 @@ function navBuilder(sections) {
  */
 
 // Build menu 
-navBuilder(mSections)
+
+navBuilder(mSections);
 // Scroll to section on link click
-observer = new IntersectionObserver(handleIntersection,Options);
-for(let section of mSections){
-observer.observe(section);
-}
+navBar.addEventListener('click', scrollToSection)
 // Set sections as active
+observer = new IntersectionObserver(changeClassOnIntersection, Options);
+for (let section of mSections) {
+    observer.observe(section);
+}
